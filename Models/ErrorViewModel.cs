72b256013bc5fe4791aedb4f6b1ed6cad9f82011
Axel Frías Hernández <axel.frias257@gmail.com>
2021-05-08 23:39:45 -0500

    Configuración mvc de vista de proyecto historial médico

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vista_historial_medico_blockchain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
