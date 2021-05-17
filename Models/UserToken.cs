9b2e557a08b472d176f020967387af6b68e2d38f
lizhernandez <liz-hernandez-99@hotmail.com>
2021-05-17 02:17:02 -0500

    Update UserToken.cs

--- Models/UserToken.cs
+++ Models/UserToken.cs
@@ -5,8 +5,6 @@ namespace Vista_historial_medico_blockchain.Models
 {
     public class UserToken
     {
-        [JsonInclude]
-        public string UserId { get; set; }
 
         [JsonInclude]
         public string Token { get; set; }
