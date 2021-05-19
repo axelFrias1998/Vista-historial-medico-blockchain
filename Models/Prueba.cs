92b2edd4f0c5537a6f7599d846faedcaa6fccf28
HidaiHdez <hhernandezh1402@alumno.ipn.mx>
2021-05-19 01:31:44 -0500

    Create Prueba.cs

---
+++ Models/Prueba.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Vista_historial_medico_blockchain.Models
+{
+    public class Prueba
+    {
+        
+        public ServicesCatalog ServicesCatalog {get; set;}
+
+        public HospitalInfo HospitalInfo {get; set;}
+        
+    }
+}
