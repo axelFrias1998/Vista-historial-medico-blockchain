73740040f05d82ed34f01592dd963bf8abd2dcee
lizhernandez <liz-hernandez-99@hotmail.com>
2021-05-19 01:46:09 -0500

    Create SpecialitiesCatalog.cs

---
+++ Models/SpecialitiesCatalog.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace Vista_historial_medico_blockchain.Models
+{
+    public partial class SpecialitiesCatalog
+    {
+        public SpecialitiesCatalog()
+        {
+            //Hospitals = new HashSet<Hospital>();
+        }
+
+        [JsonInclude]
+        public int Id { get; set; }
+
+        [JsonInclude]
+        public string Type { get; set; }
+
+        [JsonInclude]
+        public bool Hospitals { get; set; }
+
+        //public virtual ICollection<Hospital> Hospitals { get; set; }
+    }
+}
