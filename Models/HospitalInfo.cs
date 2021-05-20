b76be8e719e8bb3de6cb47f11bc4c317364852c2
HidaiHdez <hhernandezh1402@alumno.ipn.mx>
2021-05-19 20:17:40 -0500

    Update HospitalInfo.cs

--- Models/HospitalInfo.cs
+++ Models/HospitalInfo.cs
@@ -7,20 +7,22 @@ namespace Vista_historial_medico_blockchain.Models
     public class HospitalInfo
     {
 
-        [JsonInclude]
+        
         [MaxLength(150, ErrorMessage = "El Nombre de usuario debe tener como máximo 150 caracteres")]
         [MinLength(3, ErrorMessage = "El Nombre de usuario debe tener al menos 3 caracteres")]
         public string Name { get; set; }
 
-        [JsonInclude]
+        
         [MaxLength(20, ErrorMessage = "El Teléfono de usuario debe tener como máximo 20 caracteres")]
         [MinLength(0, ErrorMessage = "El Teléfono de usuario debe tener al menos 1 caracter")]
         public string PhoneNumber { get; set; }
 
-        [JsonInclude]
+        
         public int ServiceCatalogId { get; set; }
 
-        [JsonInclude]
+        
        public string AdminId { get; set; }
+
+       public virtual ServicesCatalog ServiceCatalog { get; set; }
     }
 }
