d4c6a076f3ad35528c4b4b417c35be4c615e59ba
lizhernandez <liz-hernandez-99@hotmail.com>
2021-05-20 00:16:45 -0500

    Create UserController.cs

---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Vista_historial_medico_blockchain.Models;
+
+namespace Vista_historial_medico_blockchain.Controllers
+{
+    public class UserController : Controller
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public UserController()
+        {
+
+        }
+
+        public IActionResult AdminInfo()
+
+        {
+
+            return View();
+
+        }
+
+         public IActionResult CreateAdmin()
+
+        {
+
+            return View();
+
+        }
+
+    }
+}
