01862d41a00029541ebe3dfed8dfa9aa2a9af8d9
lizhernandez <liz-hernandez-99@hotmail.com>
2021-05-20 00:16:30 -0500

    Create CatalogosController.cs

---
+++ Controllers/CatalogosController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Vista_historial_medico_blockchain.Models;
+
+namespace Vista_historial_medico_blockchain.Controllers
+{
+    public class CatalogosController : Controller
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public CatalogosController()
+        {
+
+        }
+
+        public IActionResult Catalogos()
+
+        {
+
+            return View();
+
+        }
+
+        public IActionResult CreateSpecialities()
+
+        {
+
+            return View();
+
+        }
+
+    }
+}
