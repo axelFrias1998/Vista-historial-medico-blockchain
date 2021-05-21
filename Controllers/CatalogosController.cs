833e97e35781e397b7b941db65d38a3bc44b913f
HidaiHdez <hhernandezh1402@alumno.ipn.mx>
2021-05-20 22:43:44 -0500

    Update CatalogosController.cs
    Get Tipo

--- Controllers/CatalogosController.cs
+++ Controllers/CatalogosController.cs
@@ -1,7 +1,10 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -20,15 +23,36 @@ namespace Vista_historial_medico_blockchain.Controllers
         {
 
         }
-
-        public IActionResult Catalogos()
+        public async Task<ActionResult> Catalogos()
 
         {
+            HttpResponseMessage response = await client.GetAsync("https://historial-blockchain.azurewebsites.net/api/CatalogOfServices");
+            response.EnsureSuccessStatusCode();
 
-            return View();
+            if (!response.IsSuccessStatusCode)
+                return NotFound();
+            
+            return View(JsonConvert.DeserializeObject<List<ServicesCatalog>>(await client.GetStringAsync("https://historial-blockchain.azurewebsites.net/api/CatalogOfServices")).ToList());
+                      
 
         }
 
+        public async Task<ActionResult> Especia()
+
+        {
+            HttpResponseMessage response = await client.GetAsync("https://historial-blockchain.azurewebsites.net/api/SpecialitiesCatalog");
+            response.EnsureSuccessStatusCode();
+
+            if (!response.IsSuccessStatusCode)
+                return NotFound();
+            
+            return View(JsonConvert.DeserializeObject<List<SpecialitiesCatalog>>(await client.GetStringAsync("https://historial-blockchain.azurewebsites.net/api/SpecialitiesCatalog")).ToList());
+                      
+
+        }
+
+
+    
         public IActionResult CreateSpecialities()
 
         {
