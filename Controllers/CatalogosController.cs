9eaffb6a9b7f7b7719a6e3d30170d2fd5c18a3b8
HidaiHdez <hhernandezh1402@alumno.ipn.mx>
2021-06-08 12:25:40 -0500

    Update CatalogosController.cs
    Btn nuevo editar

--- Controllers/CatalogosController.cs
+++ Controllers/CatalogosController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
@@ -9,9 +10,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
 using Newtonsoft.Json;
 using Vista_historial_medico_blockchain.Models;
-
+using System.Net.Http.Headers;
 namespace Vista_historial_medico_blockchain.Controllers
 {
     public class CatalogosController : Controller
@@ -22,18 +24,27 @@ namespace Vista_historial_medico_blockchain.Controllers
         {
 
         }
-        public async Task<ActionResult> Catalogos()
+        
+        [HttpGet]
+        public async Task<ActionResult> Catalogos(ServicesCatalog servicesCatalog)
 
         {
-            HttpResponseMessage response = await client.GetAsync("https://localhost:44349/api/CatalogOfServices");
-            response.EnsureSuccessStatusCode();
-
-            if (!response.IsSuccessStatusCode)
-                return NotFound();
-            
-            return View(JsonConvert.DeserializeObject<List<ServicesCatalog>>(await client.GetStringAsync("https://localhost:44349/api/CatalogOfServices")).ToList());             
+           using(var client = new HttpClient()){
+                /*Mandar Token en el Header*/
+                var ck = ControllerContext.HttpContext.Request.Cookies["Token"];
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ck);
+                client.BaseAddress = new Uri("https://localhost:44349");
+                var response = await client.GetAsync("api/CatalogOfServices");
+                if (response.IsSuccessStatusCode){
+                     return View(JsonConvert.DeserializeObject<List<ServicesCatalog>>(await client.GetStringAsync("https://localhost:44349/api/CatalogOfServices")).ToList());  ;
+                }
+                else{
+                    return NotFound();
+                }
+            }
         }
 
+        [HttpGet]
         public async Task<ActionResult> Especia()
 
         {
@@ -47,30 +58,7 @@ namespace Vista_historial_medico_blockchain.Controllers
                       
 
         }
-
-
-        /*public async Task<ActionResult>  CreateSpecialities( ServicesCatalog serviceCatalog)
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("https://localhost:44349");
-                var postTask = client.PostAsJsonAsync<UserInfo>("api/Accounts/CreateAccount", userinfo);
-                postTask.Wait();
-
-                var result = postTask.Result;
-                if (result.IsSuccessStatusCode)
-                {
-                    return RedirectToAction("Especia");
-                }
-            }
-
-            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
-
-            return View(userinfo);
-        }*/
-    
-       
-
+                  
         public IActionResult Specialities()
 
         {
@@ -79,23 +67,87 @@ namespace Vista_historial_medico_blockchain.Controllers
 
         }
 
+        [HttpPost]
+        public async Task<ActionResult> CreateSpecialities (SpecialitiesCatalog specialitiescatalog)
+        {
+            using(var client = new HttpClient())
+            {
+                var ck = ControllerContext.HttpContext.Request.Cookies["Token"];
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ck);
+                client.BaseAddress = new Uri("https://localhost:44349");
+                var postTask = await client.PostAsJsonAsync<SpecialitiesCatalog>("api/SpecialitiesCatalog",specialitiescatalog);
+
+                if (postTask.IsSuccessStatusCode)
+                {
+                   return View("Especia");
+                }
+                else{
+                    ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+                    return View("UserInfo");
+                }
+               
+            }
+
+        }
         
-        /*[HttpPost]
-        [ValidateAntiForgeryToken]*/
-         public async Task<ActionResult> CreateSpecialities(SpecialitiesCatalog specialitiescatalog)
+
+        [HttpPut]
+        public async Task<ActionResult> EditSpecia(SpecialitiesCatalog specialitiesCatalog)
         {
             using (var client = new HttpClient())
             {
+                var ck = ControllerContext.HttpContext.Request.Cookies["Token"];
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ck);
                 client.BaseAddress = new Uri("https://localhost:44349");
-                var postTask = client.PostAsJsonAsync<SpecialitiesCatalog>("api/SpecialitiesCatalog", specialitiescatalog);
-                postTask.Wait();
-                var result = postTask.Result;
-                if (result.IsSuccessStatusCode)
-                {
-                    var userToken = JsonConvert.DeserializeObject<UserToken>(await result.Content.ReadAsStringAsync());
+                var response = await client.GetAsync($"api/SpecialitiesCatalog/{specialitiesCatalog.Id}/{specialitiesCatalog.Nombre}");
+                if (response.IsSuccessStatusCode){
+                    return View("Especia");
+                }
+                else{
+                    ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+                    return View("EditSpecia");
+                } 
+            }
+        }
+
+            [HttpGet]
+       /* public async Task<ActionResult> Especia(SpecialitiesCatalog specialitiesCatalog)
+
+        {
+           using(var client = new HttpClient()){
+                
+                var ck = ControllerContext.HttpContext.Request.Cookies["Token"];
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ck);
+                client.BaseAddress = new Uri("https://localhost:44349");
+                var response = await client.GetAsync($"api/SpecialitiesCatalog/{specialitiesCatalog.Id}");
+                if (response.IsSuccessStatusCode){
+                     return View(JsonConvert.DeserializeObject<List<SpecialitiesCatalog>>(await client.GetStringAsync("https://localhost:44349/api/api/SpecialitiesCatalog/{specialitiesCatalog.Id}")).ToList());  ;
+                }
+                else{
+                    return NotFound();
                 }
             }
-            return View(specialitiescatalog);
+        }*/
+
+     [HttpDelete]
+     public async Task<ActionResult> Delete(SpecialitiesCatalog specialitiesCatalog)
+        {
+            using (var client = new HttpClient())
+            {
+                var ck = ControllerContext.HttpContext.Request.Cookies["Token"];
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ck);
+                client.BaseAddress = new Uri("https://localhost:44349");
+                var response = await client.DeleteAsync($"api/SpecialitiesCatalog/{specialitiesCatalog.Id}");
+                if (response.IsSuccessStatusCode){
+                    return View("Especia");
+                }
+                else{
+                    ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+                    return View("Especia");
+                } 
+            }
         }
+
+       
     }
 }
