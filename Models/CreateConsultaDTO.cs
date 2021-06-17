5625addf356606a869eacf0a44f1ba8ce174749e
HidaiHdez <hhernandezh1402@alumno.ipn.mx>
2021-06-16 20:13:52 -0500

    Create CreateConsultaDTO.cs
    Modelo para consultas

---
+++ Models/CreateConsultaDTO.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace Vista_historial_medico_blockchain.Models
+{
+    public partial class CreateConsultaDTO
+    {
+        
+        [JsonInclude]
+        public string pacienteId { get; set; }
+
+        [JsonInclude]
+        public string doctorId{ get; set; }
+
+        [JsonInclude]
+        public string hospitalId{ get; set; }
+
+        [JsonInclude]
+        public string consultaMedica { get; set; }
+
+        [JsonInclude]
+        public string genNodeId { get; set; }
+
+    }
+}
