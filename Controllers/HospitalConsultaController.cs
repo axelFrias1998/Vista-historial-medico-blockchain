e3bf9d24b41361716238504c739d89ff526fa440
HidaiHdez <hhernandezh1402@alumno.ipn.mx>
2021-06-16 20:13:40 -0500

    Create HospitalConsultaController.cs
    Controlador para consultas

---
+++ Controllers/HospitalConsultaController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Vista_historial_medico_blockchain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Vista_historial_medico_blockchain.Controllers
+{
+    public class HospitalConsultaController : Controller
+    { 
+       
+
+        public IActionResult CrearConsulta()
+        {
+            return View();
+        }
+
+       public IActionResult Registrer()
+        {
+            return View();
+        }
+
+       
+
+
+        
+        
+        
+    }
+}
