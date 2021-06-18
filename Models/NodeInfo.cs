a92fd090ed0670e1dbca7d52cb35c95af1c9a522
lizhernandez <liz-hernandez-99@hotmail.com>
2021-06-18 14:48:08 -0500

    Nodeinfo

---
+++ Models/NodeInfo.cs
@@ -0,0 +1,10 @@
+
+namespace Vista_historial_medico_blockchain.Models
+{
+    public class NodeInfo
+    {
+        public string PacientId { get; set; }
+
+        public string GenNode { get; set; }
+    }
+}
