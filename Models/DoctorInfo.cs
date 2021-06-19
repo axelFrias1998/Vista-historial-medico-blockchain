1f403225d9bdcdf6aa3e181ab7a01a353bf5628d
lizhernandez <liz-hernandez-99@hotmail.com>
2021-06-19 01:15:40 -0500

    Create DoctorInfo.cs

---
+++ Models/DoctorInfo.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace Vista_historial_medico_blockchain.Models{
+
+    public partial class DoctorInfo{
+
+        [JsonInclude]
+        public string Doctor { get; set; }
+
+        [JsonInclude]
+
+        public string Nombre { get; set; }
+
+        [JsonInclude]
+        public string Apellido { get; set; }
+
+        [EmailAddress(ErrorMessage = "Debe ingresar un email válido")]
+        [JsonInclude]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [JsonInclude]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare("Password")]
+        [JsonInclude]
+        public string ConfirmPassword { get; set; }
+
+        [JsonInclude]
+        public string PhoneNumber { get; set; }
+
+        [JsonInclude]
+        public string UserName { get; set; }
+
+        [JsonInclude]
+        public string AdminId { get; set; }
+
+        [JsonInclude]
+        public string EspecialidadId { get; set; }
+    }
+}
